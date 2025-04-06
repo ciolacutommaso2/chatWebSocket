d9fea591b23ae24432f90b6976e18acd2890320c
tommaso <ciolacutommaso@itis-molinari.eu>
2025-04-06 22:17:34 +0200

    --

--- public/script.js
+++ public/script.js
@@ -2,36 +2,90 @@ const input = document.getElementById("input");
 const button = document.getElementById("sendButton");
 const chat = document.getElementById("chat");
 
-const template = "<li class=\"list-group-item\">%MESSAGE</li>";
+const template = "<li class=\"list-group-item\">%MES</li>";
 const messages = [];
 
 const socket = io();
+const modal = document.getElementById('myModal');
+const openModalButton = document.getElementById('openModalButton');
+const closeModalButtons = document.querySelectorAll('#closeModalButton, #closeModalButtonFooter');
+const submitNameButton = document.getElementById('submitName');
+const nomeUtenteInput = document.getElementById('nomeUtente');
+let f = false;
+
+
+const openModal = () => {
+    if(!f){
+        modal.classList.add('show');
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden'; 
+        document.body.classList.add('modal-open'); 
+    }
+};
+
+const closeModal = () => {
+    modal.classList.remove('show');
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+    document.body.classList.remove('modal-open');
+};
+openModal();
+
+document.getElementById("openModalButton").onclick = ()=> {
+    if (!f) {
+        openModal(); //solo se l'utente non ha ancora inserito il nome
+    }
+};
+
+document.getElementById("closeModalButton").onclick= () => {
+    closeModal();
+}
+
+
+document.getElementById("submitName").onclick = () => {
+  //user entra
+    const nome = nomeUtenteInput.value
+    if (nome) {
+        f = true;
+        socket.emit("name", nome)
+        console.log('Nome inserito:', nome);
+    }
+    closeModal(); 
+};
+
 
 input.onkeydown = (event) => {
-  
-  if (event.keyCode === 13) {
-      event.preventDefault();
-      button.click();
-  }
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        button.click();
+    }
 }
 
 button.onclick = () => {
-  socket.emit("message", input.value);
-  input.value = "";
+    socket.emit("message", input.value);
+    input.value = "";
 }
 
 socket.on("chat", (message) => {
-  console.log(message);
-  messages.push(message);
-  render();
+    console.log(message);
+    messages.push(message);
+    render();
 })
 
+socket.on("list", (users) => {
+    const usElement = document.getElementById("userList");
+    usElement.innerHTML = '';
+    users.forEach(user => {
+        li.textContent = user.name;
+    });
+});
+
 const render = () => {
-  let html = "";
-  messages.forEach((message) => {
-    const row = template.replace("%MESSAGE", message);
-    html+=row;
-  });
-  chat.innerHTML = html;
-  window.scrollTo(0, document.body.scrollHeight);
+    let html = "";
+    messages.forEach((message) => {
+        const row = template.replace("%MES", message);
+        html += row;
+    });
+    chat.innerHTML = html;
+  
 }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const { Server } = require('socket.io'); // importazione oggetto Server da socket.io
 const conf = JSON.parse(fs.readFileSync("./conf.json"));
 
+const users = [];
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -18,11 +19,42 @@ app.use("/", express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
 const io = new Server(server);
 io.on('connection', (socket) => {
-   console.log("socket connected: " + socket.id);
-   io.emit("chat", "new client: " + socket.id);
-   socket.on('message', (message) => {
-      const response = socket.id + ': ' + message;
-      console.log(response);
-      io.emit("chat", response);
-   });
+    console.log("socket connected: " + socket.id);
+    io.emit("list", users);
+
+    socket.on('name', (name) => {
+        //add usente
+        users.push({ socketId : socket.id, name : name}) 
+        io.emit("list", users);
+    })
+
+    socket.on('message', (message) => {
+    //quando mutente manda mess
+      users.forEach((us) => {
+        if (us.socketId === socket.id) {
+            const user = us;
+        }
+      })
+      
+      if (user) {
+            const res = `${user.name}: ${message}`;
+            io.emit("chat", res); 
+        }
+    });
+    socket.on('disconnect', () => { 
+        console.log("disconnect-> " + socket.id);
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].socketId === socket.id) {
+                users.splice(i, 1);
+                break; 
+            }
+        }
+
+        io.emit("list", users);
+    });
+});
+
+server.listen(conf.port, () => {
+  console.log("server running on port: " + conf.port);
+
 });
